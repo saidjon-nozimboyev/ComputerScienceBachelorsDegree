ede4d3ef7d3c7f824f7d8463fdf890f7d2f44a90
Saidjon <saidjonnozimboyevv@gmail.com>
2025-01-16 23:10:55 -0500

    minor change

--- main.c
+++ main.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-    printf("Hello World\n");
-    printf("Hello Ali");
+    int a;
+    printf("Ali please enter number: ");
+    scanf("%i", &a);
+    printf("Your number is %i", a);
     return 0;
 }
