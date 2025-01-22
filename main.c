e12eb1bbb3959e15c46f85cbb0fe42523a389705
Saidjon <saidjonnozimboyevv@gmail.com>
2025-01-21 23:34:27 -0500

    math1720added

--- main.c
+++ main.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 
+void SayHello(void);
+
 int main()
 {
-    int a;
+    SayHello();
+    int a,b;
     printf("Ali please enter number: ");
     scanf("%i", &a);
     printf("Your number is %i", a);
     return 0;
+}
+
+void SayHello(void)
+{
+    printf("Hello Ali\n");
+    printf("You are in function call\n");
 }
