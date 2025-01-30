9d77f908d922cb57dbef74908afacd0a2969fb92
Saidjon <saidjonnozimboyevv@gmail.com>
2025-01-30 12:31:21 -0500

    test prefix postfix

--- FirstYear/COMP1400/Code/test.c
+++ FirstYear/COMP1400/Code/test.c
@@ -2,9 +2,13 @@
 
 int main(){
     int a = 10;
-    int b = 15;
-    if (a = b){
-        printf("Wokred");
-    }
-    
-}   
+    int b = 5;
+    // if (a = b){
+    //     printf("Wokred\n");
+    // }
+    // printf("%d\n",++a);
+    // printf("%d",a++);
+    int c = ++a - b--;
+    printf("%d",c);
+
+}
