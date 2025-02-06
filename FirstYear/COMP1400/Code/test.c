b45f3194aa19786c18cb40a4a5fe1ca223fe8a6b
Saidjon <saidjonnozimboyevv@gmail.com>
2025-02-05 19:03:43 -0500

    comment

--- FirstYear/COMP1400/Code/test.c
+++ FirstYear/COMP1400/Code/test.c
@@ -4,11 +4,11 @@ int main()
 {
     int a = 10;
     int b = 5;
-    // if (a = b){
-    //     printf("Wokred\n");
-    // }
-    // printf("%d\n",++a);
-    // printf("%d",a++);
+    if (a = b){
+        printf("Wokred\n");
+    }
+    printf("%d\n",++a);
+    printf("%d",a++);
     int c = ++a - b--;
     printf("%d",c);
 
