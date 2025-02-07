e7c8c47529bcf1d8888b5695c5049e7312a3cb93
Saidjon <saidjonnozimboyevv@gmail.com>
2025-02-07 08:27:56 -0500

    Comp1400Lab4PartA

---
+++ FirstYear/COMP1400/Lab4/PartA/Exercise2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello!");
+    printf("My name is: \** Saidjon \**");
+    printf("Field of Study: \" Computer Science\"");
+    printf("Thanks :)");
+}
---
+++ FirstYear/COMP1400/Lab4/PartA/Exercise4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("x value \t expressions \t resilts\n");
+    printf("| 5   | \t| y = x+ 3 | \t| y = 8 |\n");
+    printf("| 10  | \t| y = x+ 5 | \t| y = 15|\n");
+}
