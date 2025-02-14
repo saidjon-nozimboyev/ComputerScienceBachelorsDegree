3f2925262f9c55b577b212879fa036a5e1d336e6
Saidjon <saidjonnozimboyevv@gmail.com>
2025-02-14 12:36:26 -0500

    COMP1400Lab5Exe5

--- FirstYear/COMP1400/Lab5/PartA/Exercise2.c
+++ FirstYear/COMP1400/Lab5/PartA/Exercise2.c
@@ -4,9 +4,9 @@ int main()
 {
     int num;
     printf("Please enter the number:");
-    scanf("%i",&num);
-    if (num%2 & 0)
-        printf("%i is even",num);
+    scanf("%i", &num);
+    if (num % 2 == 0)
+        printf("%i is even", num);
     else
-        printf("%i is odd",num);
+        printf("%i is odd", num);
 }
---
+++ FirstYear/COMP1400/Lab5/PartB/Exercise5.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+
+int main()
+{
+    long long isbn;
+    printf("Please enter ISBN: ");
+    scanf("%lld", &isbn);
+
+    // Get the number of digits
+    int digit_count = 0;
+    long long reverse = 0;
+    while (isbn != 0)
+    {
+        short digit = isbn % 10;
+        reverse = reverse * 10 + digit;
+        isbn /= 10;
+        digit_count++;
+    }
+    printf("Digit count: %i\n", digit_count);
+    printf("Reversed: %lld\n", reverse);
+    // *1 when digit_count % 2 == 1
+    // *3 when diigit_count % 2 == 0
+    int sum = 0;
+    if (digit_count % 2 == 0)
+    {
+        short switcher = 1;
+        while (reverse != 0)
+        {
+            short digit = reverse % 10;
+            if (switcher)
+            {
+                sum += digit * 1;
+                switcher = 0;
+            }
+            else
+            {
+                sum += digit * 3;
+                switcher = 1;
+            }
+            reverse /= 10;
+        }
+        printf("Sum is: %i\n", sum);
+        printf("Check digit is: %i", (10 - (sum % 10)));
+    }
+    else if (digit_count % 2 == 1)
+    {
+        short switcher = 1;
+        while (reverse != 0)
+        {
+            short digit = reverse % 10;
+            if (switcher)
+            {
+                sum += digit * 1;
+                switcher = 0; 
+            }
+            else
+            {
+                sum += digit * 3;
+                switcher = 1; 
+            }
+            reverse /= 10;
+        }
+        printf("Sum is: %i\n", sum);
+        printf("Check digit is: %i", (10 - (sum % 10)) % 10);
+    }
+}
