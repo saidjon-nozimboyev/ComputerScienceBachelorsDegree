6e30ec98e2a201dd782f46d362a92b05b2d713b0
Saidjon <saidjonnozimboyevv@gmail.com>
2025-03-06 21:53:00 -0500

    Donut code added :)

--- FirstYear/COMP1400/Code/test.c
+++ FirstYear/COMP1400/Code/test.c
@@ -1,18 +1,47 @@
 #include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <unistd.h>
 
-int main()
-{
-    int a = 10;
-    int b = 5;
-    if (a = b){
-        printf("Wokred\n");
+int main() {
+    float A = 0, B = 0;
+    float i, j;
+    int k;
+    float z[1760];
+    char b[1760];
+    printf("\x1b[2J");
+    for (;;) {
+        memset(b, 32, 1760);  // Fill with spaces
+        memset(z, 0, 7040);   // Clear z-buffer
+        for (j = 0; 6.28 > j; j += 0.07) {
+            for (i = 0; 6.28 > i; i += 0.02) {
+                float c = sin(i);
+                float d = cos(j);
+                float e = sin(A);
+                float f = sin(j);
+                float g = cos(A);
+                float h = d + 2;
+                float D = 1 / (c * h * e + f * g + 5);
+                float l = cos(i);
+                float m = cos(B);
+                float n = sin(B);
+                float t = c * h * g - f * e;
+                int x = 40 + 30 * D * (l * h * m - t * n);
+                int y = 12 + 15 * D * (l * h * n + t * m);
+                int o = x + 80 * y;
+                int N = 8 * ((f * e - c * d * g) * m - c * d * e - f * g - l * d * n);
+                if (22 > y && y > 0 && x > 0 && 80 > x && D > z[o]) {
+                    z[o] = D;
+                    b[o] = ".,-~:;=!*#$@"[N > 0 ? N : 0];
+                }
+            }
+        }
+        printf("\x1b[H");
+        for (k = 0; 1761 > k; k++)
+            putchar(k % 80 ? b[k] : 10);  // Print line by line
+        A += 0.04;  // Rotate around X-axis
+        B += 0.02;  // Rotate around Z-axis
+        usleep(9000);  // Slow down animation
     }
-    printf("%d\n",++a);
-    printf("%d",a++);
-    int c = ++a - b--;
-    printf("%d",c);
-    // test
-    // i've pulled it from github
-    // succesfully pulled, now pushing
-    // switched to dev branch and now will push from dev branch
+    return 0;
 }
