6eec5be02546aff72136d5591a26df7217ac7b7e
Saidjon <saidjonnozimboyevv@gmail.com>
2025-10-20 23:13:46 -0400

    Lab5Task1Done

--- FirstYear/COMP1410/Labs/Lab5/Task1.c
+++ FirstYear/COMP1410/Labs/Lab5/Task1.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
 
+void computeRowAverage(int production[3][4])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < 4; j++)
+        {
+            sum += production[i][j];
+        }
+        printf("Product %d : %.1f \n", i + 1, sum / 4.0);
+    }
+}
+
 int main()
 {
     int product[3][4];
@@ -10,15 +23,13 @@ int main()
             scanf("%d", &product[i][j]);
         }
     }
-
-    // Output values
-    printf("\nThe 3x4 array is:\n");
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 4; j++)
-        {
-            printf("%d ", product[i][j]);
-        }
-        printf("\n"); // move to next row
-    }
+    computeRowAverage(product);
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 4; j++)
+    //     {
+    //         printf("%d ", product[i][j]);
+    //     }
+    //     printf("\n");
+    // }
 }
