6f9aa0d322961d0dba4d48bf63591dd145afa849
Saidjon <saidjonnozimboyevv@gmail.com>
2025-10-30 00:11:23 -0400

    COMP1410 Assignment started

---
+++ FirstYear/COMP1410/Assignments/Assignment1/Assignment1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Weighted Grade Calculation Program: \n");
+    printf("===================================\n");
+    printf("1. Single Student Grades\n");
+    printf("2. Multiple Students Grades\n");
+    printf("3. Exit");
+    printf("===================================\n");
+    short choice;
+    printf("Please enter your choice: ");
+    scanf("%hd", &choice);
+
+    
+
+    switch (choice)
+    {
+    case 1:
+        
+        break;
+    
+    default:
+        break;
+    }
+}
