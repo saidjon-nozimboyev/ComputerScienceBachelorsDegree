b70587ef5cefc770bc035438d4c44ea8d3f99700
Saidjon <saidjonnozimboyevv@gmail.com>
2026-01-13 23:38:55 -0500

    JavaInstalledAndTested

---
+++ SecondYear/COMP2120/Code/Main.java
@@ -0,0 +1,14 @@
+public class Main 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World");
+        System.out.print("Counting: ");
+        int ans = Count(5,6);
+        System.out.print(ans);
+    }   
+    public static int Count(int a, int b)
+    {
+        return a+b;
+    }
+}
