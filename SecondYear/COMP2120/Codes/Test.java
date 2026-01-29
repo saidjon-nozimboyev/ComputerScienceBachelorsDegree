2e9a6c57ea7c7d74e9b6434911fbf1d957f06214
Saidjon <saidjonnozimboyevv@gmail.com>
2026-01-28 23:04:28 -0500

    Math1730Lecture

---
+++ SecondYear/COMP2120/Codes/Test.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Test
+{
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        // System.err.print("Enter number: ");
+        // int a = input.nextInt();
+        // System.out.println("Number: "+a);    
+
+        String str = input.nextLine();
+        System.err.println("Str: "+str); 
+    }
+}
